303a3e2ade3c325c553366c68051a6fa111b0413
huangjinhe1 <huangjinhe1@jd.com>
2023-02-11 22:59:11 +0800

    init

---
+++ src/main/java/com/lm/tool/ReceiveInfo.java
@@ -0,0 +1,9 @@
+package com.lm.tool;
+
+public class ReceiveInfo {
+    public long uid;
+    public String uname;
+    public String receiver;
+    public String phone;
+    public String address;
+}
